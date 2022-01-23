602d0cb669ac6ab4c1ee16fc8efff611207e680e
HosseinParahi <hossein.parahi@gmail.com>
2022-01-23 18:03:18 +0530

    var addedd

--- led-color.py
+++ led-color.py
@@ -5,8 +5,9 @@ red = 'http://192.168.2.101/?r255g2b29&'
 green = 'http://192.168.2.101/?r20g97b0&'
 purple = 'http://192.168.2.101/?r142g38b169&'
 
+a = ()
 
-payload = urllib.request.urlopen(red)
+payload = urllib.request.urlopen(a)
 
 print(payload.status)
 print(payload.url)
