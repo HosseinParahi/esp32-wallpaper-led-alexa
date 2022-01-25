793a5f2541f3114785948d36db18aaedb752d9ea
HosseinParahi <hossein.parahi@gmail.com>
2022-01-25 13:57:05 +0530

    class added to win change

--- wallpaper-change-windows.py
+++ wallpaper-change-windows.py
@@ -1,3 +1,13 @@
 import ctypes
-WALLPAPER_PATH = "G:\\Library\\Documents\\VScode\\esp32-wallpaper-led-alexa\\2.jpg"
-ctypes.windll.user32.SystemParametersInfoW(20, 0, WALLPAPER_PATH, 3)
+
+class wallpaperchange:
+    def __init__(self, path, changevar):
+        
+        self.path = path
+        self.changevar=changevar
+        def changewall(self):
+            ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 3)
+
+path="G:\\Library\\Documents\\VScode\\esp32-wallpaper-led-alexa\\2.jpg"
+
+
